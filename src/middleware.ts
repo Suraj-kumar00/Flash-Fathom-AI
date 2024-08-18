6b727a13108652c49872bf1188f053437087d41d
Suraj <suraj.ukumar.p@gmail.com>
2024-08-18 20:29:01 +0530

    fix: the pricing path is fixed.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 
-const isPublicRoute = createRouteMatcher(['/','/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/','/sign-in(.*)', '/sign-up(.*)','/pricing'])
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
