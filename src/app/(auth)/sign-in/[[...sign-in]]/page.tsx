78a0e2eecf3b7c20cc044b91c32eab58699a4d0e
Suraj <suraj.ukumar.p@gmail.com>
2024-08-19 01:03:20 +0530

    fix: The footer is fixed in the sign-in and sign-up page.

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='flex items-center justify-center flex-col gap-10'>
+    <div className='flex items-center justify-center flex-col gap-10 h-screen'>
         <SignIn />
     </div>
   );
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='flex items-center justify-center flex-col gap-10'>
+    <div className='flex items-center justify-center flex-col gap-10 h-screen'>
         <SignUp />
     </div>
   );
