6bc0fbbb5fee4e7e009978b7ea8a54ca511c4803
ansiace <animeshraj80707@gmail.comgit config --global user.email>
2024-10-21 00:45:54 +0530

    some changes in signup/page.tsx

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='flex items-center justify-center flex-col gap-10 h-screen'>
+    <div className='flex items-center justify-center flex-col gap-10 h-screen lg:my-12 my-8'>
         <SignUp />
     </div>
   );
