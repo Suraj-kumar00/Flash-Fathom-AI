56e33410197f468b4c94306b90d2cb0ba90f55eb
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2024-10-27 14:00:43 +0530

    fix: The Dark Mode is Not Affecting the sign in & sign up page.

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,19 @@
 import { SignUp } from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
 
 export default function Page() {
   return (
     <div className='flex items-center justify-center flex-col gap-10 h-screen'>
+      <div className='dark:block hidden'>
+        <SignUp 
+        appearance={{
+          baseTheme: dark
+        }}
+        />
+        </div>
+        <div className='block dark:hidden'>
         <SignUp />
+        </div>
     </div>
   );
 }
